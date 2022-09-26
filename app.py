9fdc76380e6294a554135f0dbbc2ec43ee20c757
Liam Vlaskamp <lvlaskamp@gmail.com>
2022-09-26 15:25:56 +0200

    Added navigation bar and placeholder content

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/'
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
