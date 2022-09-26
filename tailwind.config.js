4d34fadb1102d8efd3814a25b55c4cb8fee38419
Liam Vlaskamp <lvlaskamp@gmail.com>
2022-09-26 12:16:18 +0200

    Added placeholder section and buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'main': '#2c5f78',
+        'main-light': '#51acc5',
+        'main-dark': '#1c3d52',
       }
     },
   },
